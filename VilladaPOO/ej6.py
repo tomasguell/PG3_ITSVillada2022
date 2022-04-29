89fd88e9ecbc89e6389d94cf711a451bee5ba0f9
Tomas Guell <tomasguell123@gmail.com>
2022-04-29 08:29:26 -0300

    ejercicio 6

---
+++ VilladaPOO/ej6.py
@@ -0,0 +1,20 @@
+"""6)Declarar una clase llamada Familia. Definir como atributos el nombre del padre, madre y una lista con los
+nombres de los hijos.
+Definir el método especial __str__ que retorne un string con el nombre del padre, la madre y de todos sus
+hijos."""
+class Familia:
+    def __init__(self, hijos):
+        self.Padre = input("Ingrese el nombre del Padre")
+        self.Madre = input("Ingrese el nombre de la Madre")
+        self.Hijos = hijos
+        
+
+    def __str__(self) :
+        StringLista = ''.join([str(item) for item in self.Hijos])
+        return "\nPadre: " + self.Padre + "\nMadre: " + self.Madre + "\nHijos: "+ StringLista
+            
+familia1 = Familia(["Facu", "Tomi"])     
+print(familia1)  
+familia2 = Familia(["Franco", "agus", "Guada"])    
+
+print(familia2)
